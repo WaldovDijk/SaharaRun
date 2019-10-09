92b3643665a9f356bc486c3fac81a376209a1cda
Wallykiller1 <waldo_vandijk@hotmail.com>
2019-10-09 13:27:16 +0200

    Push

--- SaharaRun/Assets/Waldo/scripts/EndlessGeneration.cs
+++ SaharaRun/Assets/Waldo/scripts/EndlessGeneration.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EndlessGeneration : MonoBehaviour {
+
+    [SerializeField] Transform m_Spawn;
     [SerializeField]
     private List<GameObject> m_Prefabs;
-    [SerializeField]
-    private GameObject m_Background;
     [SerializeField]
     private Transform m_SpawnPosition;
     [SerializeField]
@@ -16,10 +16,7 @@ public class EndlessGeneration : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        if (m_Background)
-        {
-            m_BackGroundWidth = m_Background.GetComponent<SpriteRenderer>().size.x;
-        }
+
 	}
 	
 	// Update is called once per frame
@@ -28,9 +25,9 @@ public class EndlessGeneration : MonoBehaviour {
 		if(transform.position.x < m_SpawnPosition.position.x)
         {
             Debug.Log("Spawn");
-            transform.position = new Vector3(transform.position.x + m_BackGroundWidth + m_Distancebetween, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + m_BackGroundWidth + m_Distancebetween, m_Spawn.transform.position.y, m_Spawn.transform.position.z);
 
-            Instantiate(m_Prefabs[Random.Range(1, m_Prefabs.Count)], transform.position, transform.rotation);
+            Instantiate(m_Prefabs[Random.Range(0, m_Prefabs.Count)], transform.position, transform.rotation);
         }
 	}
 }
