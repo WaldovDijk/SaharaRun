8e32e0e30c3d22ad164083fdb3927b4710e515fa
Malik <abdumalik@hotmail.nl>
2019-10-16 09:31:25 +0200

    Menu and Highscores added, P.S.: Artists kunnen sprites toevoegen en werkt menu nog steeds prima

---
+++ SaharaRun/Assets/Malik/Scripts/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public string m_playScene;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(m_playScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ SaharaRun/Assets/Malik/Scripts/HighScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    public Text m_scoreText;
+    private float m_score;
+    [SerializeField] private float m_speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_scoreText.text = m_score.ToString("F0");
+        AddScoresPerSec();
+    }
+
+    void AddScoresPerSec()
+    {
+        m_score += m_speed * Time.deltaTime;
+    }
+}
