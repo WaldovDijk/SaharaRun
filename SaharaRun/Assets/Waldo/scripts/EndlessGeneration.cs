bbd3f59ecf4d8d75719e3b7210fd791ab9f28d77
Wallykiller1 <waldo_vandijk@hotmail.com>
2019-10-16 10:40:18 +0200

    working stuff

--- SaharaRun/Assets/Waldo/scripts/EndlessGeneration.cs
+++ SaharaRun/Assets/Waldo/scripts/EndlessGeneration.cs
@@ -11,21 +11,14 @@ public class EndlessGeneration : MonoBehaviour {
     private Transform m_SpawnPosition;
     [SerializeField]
     private float m_Distancebetween;
-    [SerializeField]
-    private float m_BackGroundWidth;
 
-    // Use this for initialization
-    void Start () {
-
-	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		if(transform.position.x < m_SpawnPosition.position.x)
         {
-            Debug.Log("Spawn");
-            transform.position = new Vector3(transform.position.x + m_BackGroundWidth + m_Distancebetween, m_Spawn.transform.position.y, m_Spawn.transform.position.z);
+            transform.position = new Vector3(transform.position.x + m_Distancebetween, m_Spawn.transform.position.y, m_Spawn.transform.position.z);
 
             Instantiate(m_Prefabs[Random.Range(0, m_Prefabs.Count)], transform.position, transform.rotation);
         }
